ebd44af2df3e8e05b887a9a16698db270d077b4c
kazumoto <motch0214@gmail.com>
2018-12-30 00:18:45 +0900

    feat: Logging

--- src/main/kotlin/com/eighthours/sample/Application.kt
+++ src/main/kotlin/com/eighthours/sample/Application.kt
@@ -3,13 +3,18 @@ package com.eighthours.sample
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.features.CallLogging
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
+import org.slf4j.event.Level
 
 
 fun Application.module() {
+    install(CallLogging) {
+        level = Level.DEBUG
+    }
     install(Routing) {
         get("/") {
             call.respondText("Hello world.", ContentType.Text.Html)
